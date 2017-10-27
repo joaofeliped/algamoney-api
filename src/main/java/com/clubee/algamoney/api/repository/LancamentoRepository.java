873a1f804a9c0662459f717f0f8da9af6c10daa3
João Felipe <jo_5919@hotmail.com>
2017-10-27 11:53:43 -0200

    Adding delete resource and pagination for Lancamento

--- src/main/java/com/clubee/algamoney/api/repository/LancamentoRepository.java
+++ src/main/java/com/clubee/algamoney/api/repository/LancamentoRepository.java
@@ -3,7 +3,8 @@ package com.clubee.algamoney.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.clubee.algamoney.api.model.Lancamento;
+import com.clubee.algamoney.api.repository.lancamento.LancamentoRepositoryQuery;
 
-public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
 
 }
---
+++ src/main/java/com/clubee/algamoney/api/service/LancamentoService.java
@@ -0,0 +1,31 @@
+package com.clubee.algamoney.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.clubee.algamoney.api.model.Lancamento;
+import com.clubee.algamoney.api.model.Pessoa;
+import com.clubee.algamoney.api.repository.LancamentoRepository;
+import com.clubee.algamoney.api.repository.PessoaRepository;
+import com.clubee.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
+
+@Service
+public class LancamentoService {
+	
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Lancamento salvar(Lancamento lancamento) {
+		Pessoa pessoa = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
+		
+		if(pessoa == null || pessoa.isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+		
+		return lancamentoRepository.save(lancamento);
+	}
+
+}
