eab958de691bcbe03f1b275e463ff05aec84bf8c
João Felipe <jo_5919@hotmail.com>
2018-01-18 22:19:17 -0200

    Resumo de lançamento

--- src/main/java/com/clubee/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/clubee/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Pageable;
 
 import com.clubee.algamoney.api.model.Lancamento;
 import com.clubee.algamoney.api.repository.filter.LancamentoFilter;
+import com.clubee.algamoney.api.repository.projection.ResumoLancamento;
 
 public interface LancamentoRepositoryQuery {
 	
 	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
+	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable);
 
 }
--- src/main/java/com/clubee/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/clubee/algamoney/api/resource/LancamentoResource.java
@@ -30,6 +30,7 @@ import com.clubee.algamoney.api.exceptionhandler.AlgamoneyExceptionHandler.Erro;
 import com.clubee.algamoney.api.model.Lancamento;
 import com.clubee.algamoney.api.repository.LancamentoRepository;
 import com.clubee.algamoney.api.repository.filter.LancamentoFilter;
+import com.clubee.algamoney.api.repository.projection.ResumoLancamento;
 import com.clubee.algamoney.api.service.LancamentoService;
 import com.clubee.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
 
@@ -53,6 +54,12 @@ public class LancamentoResource {
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
 	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
 		return lancamentoRepository.filtrar(lancamentoFilter, pageable);
+	}
+	
+	@GetMapping(params = "resumo")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
+	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
+		return lancamentoRepository.resumir(lancamentoFilter, pageable);
 	}
 	
 	@GetMapping("/{codigo}")
