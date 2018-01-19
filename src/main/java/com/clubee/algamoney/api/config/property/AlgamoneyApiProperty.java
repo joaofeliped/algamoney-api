cf9e0707f1b0dd3d558cf22aaa2743962befddd6
João Felipe <jo_5919@hotmail.com>
2018-01-19 10:14:02 -0200

    Production

--- src/main/java/com/clubee/algamoney/api/config/property/AlgamoneyApiProperty.java
+++ src/main/java/com/clubee/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -1,7 +1,9 @@
 package com.clubee.algamoney.api.config.property;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ConfigurationProperties("algamoney")
 public class AlgamoneyApiProperty {
 	
