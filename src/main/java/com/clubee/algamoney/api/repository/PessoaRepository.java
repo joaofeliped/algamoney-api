ae58546fc715284825567d82622b55793b4fa576
João Felipe <jo_5919@hotmail.com>
2018-01-24 17:01:38 -0200

    Implements Pessoa filter with pageable

--- src/main/java/com/clubee/algamoney/api/repository/PessoaRepository.java
+++ src/main/java/com/clubee/algamoney/api/repository/PessoaRepository.java
@@ -3,7 +3,8 @@ package com.clubee.algamoney.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.clubee.algamoney.api.model.Pessoa;
+import com.clubee.algamoney.api.repository.pessoa.PessoaRepositoryQuery;
 
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>, PessoaRepositoryQuery {
 
 }
